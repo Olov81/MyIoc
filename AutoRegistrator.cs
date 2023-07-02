10eb37b0b6697b772d46bbb3d639f47c42c762cc
Olov Johansson <olov.johansson@b3.se>
2023-07-02 11:47:52 +0200

    Add interface rules in auto registrator

--- AutoRegistrator.cs
+++ AutoRegistrator.cs
@@ -2,13 +2,16 @@ namespace MyIoc;
 
 public static class RegistryExtensions
 {
-    public static void AutoRegister(this Registry registry, IEnumerable<Type> services)
+    public static void AutoRegister(
+        this Registry registry,
+        IEnumerable<Type> services,
+        Func<(Type Interface, Type Implementation), bool> shouldRegisterInterface)
     {
         foreach (var service in services)
         {
             registry.Register(service);
 
-            foreach (var @interface in service.GetInterfaces())
+            foreach (var @interface in service.GetInterfaces().Where(x => shouldRegisterInterface((x, service))))
             {
                 registry.Register(@interface, service);
             }
--- Tests/AutoRegisteringTests.cs
+++ Tests/AutoRegisteringTests.cs
@@ -10,18 +10,36 @@ public class AutoRegisteringTests
     [Fact]
     public void Should_register_all_given_types()
     {
-        _registry.AutoRegister(new[] { typeof(ServiceOne), typeof(ServiceTwo) });
+        AutoRegister(new[] { typeof(ServiceOne), typeof(ServiceTwo) });
 
-        _registry.Resolve<ServiceOne>().Should().NotBeNull();
-        _registry.Resolve<ServiceTwo>().Should().NotBeNull();
+        Resolve<ServiceOne>().Should().NotBeNull();
+        Resolve<ServiceTwo>().Should().NotBeNull();
     }
 
     [Fact]
     public void Should_register_implemented_interfaces_as_service_keys()
     {
-        _registry.AutoRegister(new[] { typeof(ServiceOne) });
+        AutoRegister(new[] { typeof(ServiceOne) });
 
-        _registry.Resolve<IServiceOne>().Should().BeOfType<ServiceOne>();
+        Resolve<IServiceOne>().Should().BeOfType<ServiceOne>();
+    }
+
+    [Fact]
+    public void Should_only_register_interfaces_fulfilling_the_specified_rule()
+    {
+        _registry.AutoRegister(new[] { typeof(ServiceOne) }, types => types.Interface.Name.Contains("Apa"));
+        
+        Assert.Throws<InvalidOperationException>(Resolve<IServiceOne>);    
+    }
+
+    private void AutoRegister(IEnumerable<Type> services)
+    {
+        _registry.AutoRegister(services, _ => true);
+    }
+
+    private TService Resolve<TService>() where TService : class
+    {
+        return _registry.Resolve<TService>();
     }
 }
 
